6e62a4349bb8da3773e5ec0c11efde71c2dc7b80
yeshwanth-raj00 <yeshr7491@gmail.com>
2026-07-26 15:36:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function facultyLogin() {
+    window.location.href = "faculty.html";
+}
+function adminLogin(){
+    window.location.href="admin.html";
+}
+function studentLogin(){
+    window.location.href="student.html";
+}
